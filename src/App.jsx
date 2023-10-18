a641b116fb77e78bae1efce2766a33a54afe5f24
tbenedictj <tbenedictjonathan@gmail.com>
2023-10-18 10:50:25 +0800

    Menambahkan Folder Card-info serta component evolution, typeeffect, dan logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import Footer from './components/footer/footer'
 import Search from './components/Home/Search/Search'
-
+import Evolution from './components/Home/Card-info/Evolution/Evolution'
+import typeEffect from './components/Home/Card-info/Type-Effect/Type-Effect'
+import LogoCI from './components/Home/Card-info/Logo/Logo'
 
 function App() {
   return (
     <div>
       <Search />
+      <Evolution />
+      <typeEffect />
+      <LogoCI />
       <Footer />
     </div>
   )
