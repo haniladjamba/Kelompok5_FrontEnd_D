7be63d47fed1ae147ac5b850a10d03e12c904d4b
Melrick1 <melrickbatuwael@gmail.com>
2023-10-18 10:29:45 +0800

    Menambahkan Folder Home > Komponen Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Footer from './components/footer/footer'
+import Search from './components/Home/Search/Search'
 
 
 function App() {
   return (
     <div>
+      <Search />
       <Footer />
     </div>
   )
