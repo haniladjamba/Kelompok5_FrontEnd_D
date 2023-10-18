021c0f78c770253bb797ce7fa1f248a3d3a4d9d5
tbenedictj <tbenedictjonathan@gmail.com>
2023-10-18 11:28:09 +0800

    Memperbaiki struktur folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 import Footer from './components/footer/footer'
 import Search from './components/Home/Search/Search'
-import cardInfo from './components/Home/Card-info/Card-Info'
+import cardInfo from './components/Home/Card-Info/Card_Info'
 import Evolution from './components/Home/Card-info/Evolution'
 import LogoCI from './components/Home/Card-info/Logo'
-import typeEffect from './components/Home/Card-info/Type-Effect'
+import typeEffect from './components/Home/Card-Info/Type_Effect'
 
 function App() {
   return (
