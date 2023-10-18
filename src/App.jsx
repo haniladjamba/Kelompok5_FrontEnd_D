0df46bfd0dd48a63bfd6453804ce376ced4a4c90
haniladjamba <s22110313@student.unklab.ac.id>
2023-10-17 18:39:46 -0700

    ganti semua file ke react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import Footer from './components/footer/footer'
-import Header from './components/footer/header/header'
 import './App.css'
 
 
 function App() {
   return (
     <div>
-      <Footer />
-      <Header />
+      
     </div>
   )
 }
