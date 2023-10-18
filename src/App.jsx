0ef344f538af768cc7de60465853178b5276d026
nmakatipu-16 <nogemanyombexxx@gmail.com>
2023-10-18 20:23:30 +0800

    menambah branch Header, folder Header, file header, file logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Footer from './components/footer/footer'
+import Header from './components/Header/header'
+import Logo from './components/Header/logo'
 
 
 function App() {
   return (
     <div>
       <Footer />
+      <Header />
+      <Logo />
     </div>
   )
 }
