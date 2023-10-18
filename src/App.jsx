ef963e16a9e275c7b5c9ef870f003b1f6268183e
Melrick1 <melrickbatuwael@gmail.com>
2023-10-18 11:40:07 +0800

    Memperbaiki file komponen Home dan Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Footer from './components/footer/footer'
-import Search from './components/Home/Search/Search'
+import Home from './components/Home/Home'
+import Search from './components/Home/Search'
 import cardInfo from './components/Home/Card-Info/Card_Info'
 import Evolution from './components/Home/Card-info/Evolution'
 import LogoCI from './components/Home/Card-info/Logo'
@@ -9,6 +10,7 @@ import typeEffect from './components/Home/Card-Info/Type_Effect'
 function App() {
   return (
     <div>
+      <Home />
       <Search />
       <cardInfo />
       <Evolution />
