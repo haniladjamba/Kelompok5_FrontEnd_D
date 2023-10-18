8f8142084d3851a72e19f49578f9b81dbd2df28c
Jerrico1209 <s2200648@student.unklab.ac.id>
2023-10-18 21:18:17 +0800

    Menambahkan file Gen1 dan Gen2 didalam folder generations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import Footer from './components/footer/footer'
 import Header from './components/Header/header'
 import Logo from './components/Header/logo'
+import Gen1 from './components/Header/generations/gen1'
+import Gen2 from './components/Header/generations/gen2'
 import Gen3 from './components/Header/generations/gen3'
 import Gen4 from './components/Header/generations/gen4'
 import Generations from './components/Header/generations/generations'
@@ -13,6 +15,8 @@ function App() {
       <Footer />
       <Header />
       <Logo />
+      <Gen1 />
+      <Gen2 />
       <Gen3 />
       <Gen4 />
       <Generations />
