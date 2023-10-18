f1757e7d97425cc7ad80c08f3cc0268d9c8c28a6
haniladjamba <s22110313@student.unklab.ac.id>
2023-10-17 18:38:12 -0700

    ganti semua files ke react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './components/footer/footer'
+import Header from './components/footer/header/header'
+import './App.css'
+
+
+function App() {
+  return (
+    <div>
+      <Footer />
+      <Header />
+    </div>
+  )
+}
+
+export default App
